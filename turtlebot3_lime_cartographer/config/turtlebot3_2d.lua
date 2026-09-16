e7f6bdea3dd8a5fda63f8661d31a10e0671541e0
Tomoaki Fujino <fujino.tomoaki404@mail.kyutech.jp>
2026-09-16 17:48:46 +0900

    fix: adjust Cartographer range limits for the LD19 lidar
    Signed-off-by: Tomoaki Fujino <fujino.tomoaki404@mail.kyutech.jp>

--- turtlebot3_lime_cartographer/config/turtlebot3_2d.lua
+++ turtlebot3_lime_cartographer/config/turtlebot3_2d.lua
@@ -46,8 +46,10 @@ options = {
 
 MAP_BUILDER.use_trajectory_builder_2d = true
 
-TRAJECTORY_BUILDER_2D.min_range = 0.12
-TRAJECTORY_BUILDER_2D.max_range = 3.5
+-- Set the range limits according to the LD19 specifications.
+-- Measurement range: 0.3 m to 12.0 m
+TRAJECTORY_BUILDER_2D.min_range = 0.3
+TRAJECTORY_BUILDER_2D.max_range = 12.0
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
